9c4a775d86364763f0012a2e170e8ce097c0620e
Zack Weinberg <zackw@cmu.edu>
2012-10-26 14:19:12 -0400

    Genesis of a test suite. Note expects Python format specs rather than printf.

---
+++ test/harness.cc
@@ -0,0 +1,108 @@
+// Test skeleton for cxxfmt.  This program is compiled, potentially
+// several times, with code injected by the Python test generators,
+// and run.  It emits per-test status messages which the Python
+// harness will process and convert to human-readable failure reports.
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fmt.h>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+using fmt::format;
+
+#define S(x) S_(x)
+#define S_(x) #x
+
+namespace {
+
+// Note: we intentionally use std::endl instead of '\n' to get cout
+// flushed after each line of output; the program is normally run
+// with cout pointing at a pipe, and the other end of the pipe wants
+// as-soon-as-possible notification of the results of each test.
+
+// Note: Plain arrays of POD structures are used because some
+// compilers are not yet very good at optimizing std::initializer_list,
+// leading to gargantuan assembly output and very slow object file
+// generation.
+
+struct case_1arg_s
+{
+  const char *spec;
+  const char *expected;
+  const char *val;
+};
+
+// more case_ structures here
+
+static void
+report(string const& label, const char *spec,
+       string const& got, const char *expected)
+{
+  if (got == expected) {
+    cout << "+\t" << label << '\t' << spec << '\t'
+         << expected << endl;
+  } else {
+    cout << "-\t" << label << '\t' << spec << '\t'
+         << expected << '\t' << got << endl;
+  }
+}
+
+template <typename case_1arg>
+static void
+process(string const& label, const case_1arg *cases, size_t n)
+{
+  for (const case_1arg *c = cases; c < cases+n; c++) {
+    string got(format(c->spec, c->val));
+    report(label, c->spec, got, c->expected);
+  }
+}
+
+// more process_ overloads here
+
+struct i_tblock { virtual void operator()(const char *) const = 0; };
+
+template <typename case_1arg>
+struct tblock : i_tblock
+{
+  template <size_t N>
+  tblock(const char *tag_, const case_1arg (&cases_)[N])
+    : tag(tag_), cases(cases_), n(N)
+  {}
+
+  virtual void operator()(const char *label_) const
+  {
+    string label(label_);
+    label += '\t';
+    label += tag;
+    cout << ":\t" << label << '\t' << n << endl;
+    process(label, cases, n);
+  }
+
+private:
+  const char *tag;
+  const case_1arg *cases;
+  size_t n;
+};
+
+#include "testcases.inc"
+
+} // anonymous namespace
+
+int
+main()
+{
+  const char compiler[] = S(COMPILER_NAME);
+
+  for (auto p = tblocks.begin(); p != tblocks.end(); p++)
+    (**p)(compiler);
+  return 0;
+}
+
+// Local Variables:
+// mode: c++
+// c-file-offsets: ((innamespace . 0))
+// End:
---
+++ test/strings.py
@@ -0,0 +1,51 @@
+#
+# Generate formatting test cases for simple string constants.
+#
+
+import lib
+
+words = [ '', 'i', 'of', 'sis', 'fice', 'drisk', 'elanet', 'hippian',
+          'botanist', 'synaptene', 'cipherhood', 'schizognath' ]
+
+aligns = [ '', '<', '>', '^', 'L<', 'R>', 'C^' ]
+
+maxw = len(words) + 3
+
+def output(spec, val):
+    return '"{}", "{}", "{}"'.format(spec, spec.format(val), val)
+
+def g_simple():
+    for w in words:
+        for a in aligns:
+            yield output('{:'+a+'}', w)
+
+def g_width():
+    for width in xrange(1, maxw):
+        formats = ['{:' + (a+'{}').format(width) + '}' for a in aligns]
+        for w in words:
+            for f in formats:
+                yield output(f,w)
+
+def g_prec():
+    for prec in xrange(maxw):
+        formats = ['{:' + (a+'.{}').format(prec) + '}' for a in aligns]
+        for w in words:
+            for f in formats:
+                yield output(f,w)
+
+def g_wnp():
+    for width in xrange(1, maxw):
+        for prec in xrange(maxw):
+            formats = ['{:' + (a+'{}.{}').format(width,prec) + '}'
+                       for a in aligns]
+            for w in words:
+                for f in formats:
+                    yield output(f,w)
+
+def generate(outf):
+    pass
+
+if __name__ == '__main__':
+    import sys
+    lib.generate_mod(sys.stdout, 'strings', 'case_1arg_s', globals())
+
