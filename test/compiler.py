74252d9f8dc6769f4e1694f0e22e529305ac7b92
Zack Weinberg <zackw@cmu.edu>
2012-12-10 13:58:53 -0500

    Polish the compiler detection logic a bunch.

--- test/compiler.py
+++ test/compiler.py
@@ -14,45 +14,79 @@ import util
 #
 
 class Compiler(object):
-    devnull = None
+    """Abstract base class for compilers installed on this computer.
+       Instances provide an API for invoking their particular
+       compiler.  The class itself provides an API for detecting
+       installed compilers and determining their characteristics."""
+
+    @util.lazy_property
+    def DEVNULL(_):
+        """Read-write file handle on /dev/null.  If possible,
+           delegates to subprocess; otherwise opens a global handle on
+           os.devnull itself.  If possible, that handle is marked
+           close-on-exec.
+
+           N.B. this is not in 'util' because descriptors do not apply
+           to lookups in module objects, and thus @lazy_property does
+           not work in that context."""
+        try:
+            return subprocess.DEVNULL
+        except AttributeError:
+            try:
+                return os.open(os.devnull, os.O_RDWR | os.O_CLOEXEC)
+            except AttributeError:
+                return os.open(os.devnull, os.O_RDWR)
 
     @staticmethod
     def identify(prog, extra_args=[]):
+        """Find out which version of which compiler 'prog' is, and which
+           C++ runtime library it offers, when invoked with 'extra_args'.
+           'prog' should be an absolute pathname to an executable."""
+
         identify_source = """\
 // We have to include some C++ library header to get the library's
 // identifying macros defined.  There are no C++ library headers that
-// define nothing but macros, so we put a marker after this inclusion
-// and strip out all the leading junk from Python.  Since we have to
-// postprocess the output anyway, this is an excuse not to bother with -P.
+// define nothing but macros, so we put a marker after the #include
+// ('%%', which is not syntactically valid C++ and therefore will not
+// appear in the expansion of the #include) and strip out everything
+// before that marker from Python.  This also gives us an opportunity
+// to strip line markers and any other junk that may appear in
+// preprocessor output.  If the compiler is already in C++11 mode,
+// we include <type_traits> instead of <iosfwd> to detect more possible
+// incompatibilities between the compiler and the library.
 
+#if __cplusplus >= 201103L
+#include <type_traits>
+#else
 #include <iosfwd>
-
-// just to be sure
-#undef F
-#undef FF
-
-#define F(a,b) FF(a,b)
-#define FF(a,b) a##.##b
+#endif
 
 %%
+
 {
 #if __cplusplus >= 201103L
   "cxx11" : 1,
 #else
   "cxx11" : 0,
 #endif
-#if defined _MSC_VER
-  "cc"    : "msvc",
-  "ccver" : 0,
-#elif defined __clang__ // N.B. clang defines __GNUC__
+// clang defines __GNUC__ and might plausibly decide to define
+// _MSC_VER on Windows, so check for it first.
+#if defined __clang__
   "cc"    : "clang",
-  "ccver" : F(__clang_major__,__clang_minor__),
+  "ccmaj" : __clang_major__,
+  "ccmin" : __clang_minor__,
 #elif defined __GNUC__
   "cc"    : "gcc",
-  "ccver" : F(__GNUC__,__GNUC_MINOR__),
+  "ccmaj" : __GNUC__,
+  "ccmin" : __GNUC_MINOR__,
+#elif defined _MSC_VER
+  "cc"    : "msvc",
+  "ccmaj" : _MSC_VER,
+  "ccmin" : 0,
 #else
   "cc"    : "unknown",
-  "ccver" : 0,
+  "ccmaj" : 0,
+  "ccmin" : 0,
 #endif
 #if defined _LIBCPP_VERSION
   "lib"   : "llvm" // "libc++" is too generic
@@ -65,12 +99,13 @@ class Compiler(object):
 #endif
 }
 """
-        if Compiler.devnull is None:
-            Compiler.devnull = open(os.devnull, "r+")
+
         try:
             # This is how g++ and clang++ want to be invoked.
-            output = util.check_io([prog, "-E", "-xc++"] + extra_args + ["-"],
-                                   stderr=Compiler.devnull,
+            argv = [prog, "-E", "-xc++"] + extra_args + ["-"]
+            #sys.stderr.write(" ".join(argv) + "\n")
+            output = util.check_io(argv,
+                                   stderr=Compiler.DEVNULL,
                                    input=identify_source)
         except subprocess.CalledProcessError:
             # Retry with appropriate switches for MSVC should go here.
@@ -78,50 +113,68 @@ class Compiler(object):
             # so it can wait.
             return { "cxx11" : 0,
                      "cc"    : "unknown",
-                     "ccver" : 0,
+                     "ccmaj" : 0,
+                     "ccmin" : 0,
                      "lib"   : "unknown" }
 
-        props = json.loads("".join(
-                  l for l in output[output.rfind("%%")+2:].splitlines()
-                    if len(l) > 0 and l[0] != '#'))
+        data = "".join(l for l in output[output.rfind("%%")+2:].splitlines()
+                       if len(l) > 0 and l[0] != '#')
+        props = json.loads(data)
+
+        props["ccver"] = str(props["ccmaj"]) + "." + str(props["ccmin"])
 
         otag = ("-" + props["cc"] +
-                "-" + str(props["ccver"]) +
-                "-lib" + props["lib"] + ".o")
+                "-" + props["ccver"] +
+                "-lib" + props["lib"])
         if os.name == "nt" or os.name == "ce":
-            otag += "bj"
+            otag += ".obj"
+        else:
+            otag += ".o"
 
         props["otag"] = otag
         return props
 
-    def __init__(self, argv, otag):
-        self.argv = argv
-        self.otag = otag
+    def __init__(self, prog, props, flags, libs):
+        """Constructor for Compiler (subclass) instances.  'prog' is
+           the name of the compiler executable.  'props' MUST be the
+           property dictionary returned by Compiler.identify(prog).
+           'flags' is a list of command line switches to pass on all
+           invocations of the compiler; 'libs' is an additional list
+           of command line switches to pass when linking."""
+        self.prog = prog
+        self.props = props
+        self.flags = flags
+        self.libs = libs
 
     def objname(self, src):
-        return os.path.splitext(src)[0] + self.otag
+        """Return an appropriately labeled name for an object file
+           compiled from source file 'src' with this compiler."""
+        return os.path.splitext(src)[0] + self.props["otag"]
 
-    # verbose=0: total silence.
-    # verbose=1: report success or failure.
-    # verbose=2: report invocation and error messages.
     def invoke(self, args, label, verbose):
+        """Invoke this compiler, passing 'args' on the command line.
+           'verbose' says how much to report about this invocation.
+           It takes one of the following numeric values:
+              0: total silence.
+              1: report success or failure.
+              2: print full command line and error messages.
+           'label' is used when verbose=1 to describe this invocation.
+           Returns True for a successful compilation, False otherwise.
+        """
         if verbose < 0 or verbose > 2:
             raise ValueError("bad verbosity {}".format(verbose))
 
-        if Compiler.devnull is None:
-            Compiler.devnull = open(os.devnull, "r+")
-
-        argv = self.argv + args
+        argv = [self.prog] + self.flags + args
         if verbose == 2:
             sys.stderr.write(" ".join(argv) + "\n")
-            rv = subprocess.call(argv, stdin=Compiler.devnull)
+            rv = subprocess.call(argv, stdin=Compiler.DEVNULL)
         else:
             if verbose == 1:
                 sys.stderr.write("{} {}...".format(argv[0], label))
             rv = subprocess.call(argv,
-                                 stdin=Compiler.devnull,
-                                 stdout=Compiler.devnull,
-                                 stderr=Compiler.devnull)
+                                 stdin=Compiler.DEVNULL,
+                                 stdout=Compiler.DEVNULL,
+                                 stderr=Compiler.DEVNULL)
         if rv == 0:
             sys.stderr.write("ok\n")
             return True
@@ -132,107 +185,134 @@ class Compiler(object):
                 sys.stderr.write("exit {}\n".format(rv))
         return False
 
-    def compile(self, src, obj, verbose=0):
+    def compile(self, src, verbose=0):
+        """Compile source file 'src' and return the name of the object
+           file generated, or False on failure.  'verbose' is passed
+           through to invoke()."""
         raise NotImplemented
 
     def link(self, objs, exe, verbose=0):
+        """Link 'objs' (a list of object file names) together to produce
+           an executable named 'exe'.  Returns a possibly-corrected executable
+           name, or False on failure; appropriate usage is
+           >>> exe = cc.link(['a.o', 'b.o'], exe)
+           'verbose' is passed through to invoke()."""
         raise NotImplemented
 
-    @staticmethod
-    def probe(prog, props):
+    @classmethod
+    def probe(cls, prog, props):
+        """Determine whether 'prog' is a usable compiler of the type
+           represented by 'cls'.  'props' MUST be the dictionary
+           returned by Compiler.identify for 'prog'.  Returns a list
+           of zero or more 'cls' instances."""
         raise NotImplemented
 
 class Unknown(Compiler):
-    @staticmethod
-    def probe(prog, props):
+    """A compiler we don't know how to drive."""
+
+    @classmethod
+    def probe(cls, prog, props):
         return []
 
 class UnixCompiler(Compiler):
-    def __init__(self, prog, flags, libs, otag):
-        Compiler.__init__(self, [prog] + flags, otag)
-        self.libs = libs
+    """A compiler whose command line conforms to Unixy conventions."""
 
-    def compile(self, src, obj, verbose=0):
-        return self.invoke(["-o", obj, "-c", src], obj, verbose)
+    def compile(self, src, verbose=0):
+        obj = self.objname(src)
+        if self.invoke(["-o", obj, "-c", src], obj, verbose):
+            return obj
+        return False
 
     def link(self, objs, exe, verbose=0):
-        return self.invoke(["-o", exe] + objs + self.libs, exe, verbose)
+        if self.invoke(["-o", exe] + objs + self.libs, exe, verbose):
+            return exe
+        return False
 
 class Clang(UnixCompiler):
-    @staticmethod
-    def probe(prog, props):
+    """A version of Clang from the LLVM project."""
+
+    @classmethod
+    def probe(cls, prog, props):
         assert props["cc"] == "clang"
 
-        if props["ccver"] < 3.0: # assumed too old
+        if props["ccmaj"] < 3: # assumed too old
             return []
 
         need_cxx11 = props["cxx11"] == 0
         try_libcxx = props["lib"] != "llvm"
         try_libgnu = props["lib"] != "gnu"
 
-        argv = []
+        flags = []
         if need_cxx11:
-            argv.append("-std=c++11")
-            if Compiler.identify(prog, argv)["cxx11"] == 0:
-                return []
-
-        argv.extend(("-g", "-O2", "-Wall", "-Wextra", "-I."))
+            flags.append("-std=c++11")
+            props = Compiler.identify(prog, flags)
+        flags.extend(("-g", "-O2", "-Wall", "-Wextra", "-I."))
 
         compilers = []
-        compilers.append(Clang(prog, argv, [], props["otag"]))
+        if props["cxx11"] == 1:
+            compilers.extend(cls(prog, props, flags, ()))
 
         if try_libcxx:
-            args = argv
-            args[1:1] = ("-stdlib=libc++",)
-            nprops = Compiler.identify(prog, args)
-            if nprops["lib"] == "llvm":
-                compilers.append(Clang(prog, args, [], nprops["otag"]))
+            nflags = flags
+            nflags[1:1] = ("-stdlib=libc++",)
+            nprops = Compiler.identify(prog, nflags)
+            if nprops["cxx11"] == 1 and nprops["lib"] == "llvm":
+                compilers.append(cls(prog, nprops, nflags, ()))
+
         if try_libgnu:
-            args = argv
-            args[1:1] = ("-stdlib=libstdc++",)
-            nprops = Compiler.identify(prog, args)
-            if nprops["lib"] == "gnu":
-                compilers.append(Clang(prog, args, [], nprops["otag"]))
+            nflags = flags
+            nflags[1:1] = ("-stdlib=libstdc++",)
+            nprops = Compiler.identify(prog, nflags)
+            if nprops["cxx11"] == 1 and nprops["lib"] == "gnu":
+                compilers.append(cls(prog, nprops, nflags, ()))
 
         return compilers
 
 class Gcc(UnixCompiler):
-    @staticmethod
-    def probe(prog, props):
+    """A version of GCC from the GNU project."""
+
+    @classmethod
+    def probe(cls, prog, props):
         assert props["cc"] == "gcc"
 
-        if props["ccver"] < 4.7: # assumed too old
+        if (props["ccmaj"] < 4 or
+            (props["ccmaj"] == 4 and props["ccmin"] < 7)): # assumed too old
             return []
 
         need_cxx11 = props["cxx11"] == 0
         try_libcxx = props["lib"] != "llvm"
 
-        argv = []
+        flags = []
         if need_cxx11:
-            argv.append("-std=c++11")
-            if Compiler.identify(prog, argv)["cxx11"] == 0:
+            flags.append("-std=c++11")
+            props = Compiler.identify(prog, flags)
+            if props["cxx11"] == 0:
                 return []
+        flags.extend(("-g", "-O2", "-Wall", "-Wextra", "-I."))
 
-        argv.extend(("-g", "-O2", "-Wall", "-Wextra", "-I."))
-
-        compilers = []
-        compilers.append(Gcc(prog, argv, [], props["otag"]))
+        compilers = [cls(prog, props, flags, ())]
 
         # GCC can be persuaded to use libc++ but it involves more work
         # than for clang.  If anyone has a better idea than this hardwired
         # list of possible locations for libc++'s headers, please let me know.
         libcxxinc = None
-        for loc in ["/", "/usr", "/usr/local", "/opt", "/opt/local"]:
-            if os.path.isfile(loc + "/include/c++/v1/iosfwd"):
-                libcxxinc = loc + "/include/c++/v1"
+        for loc in ["/include",
+                    "/usr/include",
+                    "/usr/local/include",
+                    "/opt/include",
+                    "/opt/local/include",
+                    "/opt/local/libexec/llvm-3.0/lib" # MacPorts, sigh
+                    ]:
+            if os.path.isfile(loc + "/c++/v1/iosfwd"):
+                libcxxinc = loc + "/c++/v1"
+
         if try_libcxx and libcxxinc is not None:
-            args = argv + ["-nostdinc++", "-isystem", libcxxinc]
-            nprops = Compiler.identify(prog, args)
+            nflags = flags + ["-nostdinc++", "-isystem", libcxxinc]
+            nprops = Compiler.identify(prog, nflags)
             if nprops["lib"] == "llvm":
-                compilers.append(Gcc(prog, args,
-                                     ["-nodefaultlibs",
-                                        "-lc++", "-lc", "-lgcc_s"],
-                                     nprops["otag"]))
+                compilers.append(cls(prog, nprops, nflags,
+                                     ("-nodefaultlibs",
+                                        "-lc++", "-lc", "-lgcc_s")))
         return compilers
 
 def find_compilers():
@@ -250,9 +330,8 @@ def find_compilers():
     for cmd in ["g++", "clang++"]:
         for path in pathv:
             for ver in glob.iglob(os.path.join(path, cmd) + "*"):
-                rp = os.path.realpath(ver)
-                if rp in executables: continue
-                if util.is_script(rp): continue
+                rp = util.unwrap_script(os.path.realpath(ver))
+                if not rp: continue
                 executables.add(rp)
 
     compilers = []
@@ -264,4 +343,4 @@ def find_compilers():
 if __name__ == '__main__':
     compilers = find_compilers()
     for cc in compilers:
-        cc.compile("fmt.cc", cc.objname("fmt.cc"), verbose=1)
+        cc.compile("fmt.cc", verbose=1)
--- test/util.py
+++ test/util.py
@@ -1,13 +1,63 @@
 # Test utility functions.
 
+import contextlib
+import os
+import os.path
 import subprocess
+import tempfile
+
+@contextlib.contextmanager
+def mkstemp_autodel(suffix="", prefix="tmp", dir=None, text=False,
+                    contents=None):
+    (handle, pathname) = tempfile.mkstemp(suffix, prefix, dir, text)
+    if contents is not None:
+        os.write(handle, contents)
+    os.close(handle)
+    yield pathname
+    os.unlink(pathname)
 
 def is_script(path):
-    with open(path, "r") as f:
-        return f.read(2) == "#!"
+    """Returns False if 'path' is not a script,
+       True if it's a script but not a shell script,
+       or the name of the shell interpreter if it's a shell script."""
+
+    with open(path, "rU") as f:
+        # check for #! before attempting to read up to a newline.
+        if f.read(2) != "#!": return False
+        interpreter = f.readline().split()[0]
+        if interpreter.endswith("sh") and not interpreter.endswith("csh"):
+            return interpreter
+        return True
+
+def unwrap_script(path):
+    """If 'path' is a shell script (as determined by is_script),
+       attempt to extract and return the name of the program it
+       actually runs.  Returns False if 'path' is a script but it
+       couldn't figure out what the script runs.  If 'path' is not a
+       script, returns it unchanged."""
+    interpreter = is_script(path)
+    if interpreter is False: return path
+    if interpreter is True: return False
+
+    # Definitely a shell script at this point.
+    with mkstemp_autodel(suffix=".cc", text=True) as src:
+        cmds = subprocess.check_output([interpreter, "-x", path, "-E", src],
+                                       stderr=subprocess.STDOUT)
+        for line in cmds.splitlines():
+            if line[0] == "+" and src in line:
+                for word in line.split():
+                    if word != "+" and word != "exec":
+                        if word.startswith("/"):
+                            canon = os.path.realpath(word)
+                            if os.path.isfile(canon): return canon
+                        return False
 
 # This should be in subprocess, but it ain't.
 def check_io(*popenargs, **kwargs):
+    """Same as subprocess.check_output() but takes an additional argument,
+       'input', allowing you to pass a string to the subprocess's stdin.
+       You may not use the 'stdin' and 'input' arguments at the same time."""
+
     if 'stdout' in kwargs:
         raise ValueError('stdout argument not allowed, it will be overridden.')
     if 'input' in kwargs:
@@ -27,3 +77,21 @@ def check_io(*popenargs, **kwargs):
             cmd = popenargs[0]
         raise subprocess.CalledProcessError(retcode, cmd, output=output)
     return output
+
+# credit to stackoverflow user 'Obtuse':
+# http://stackoverflow.com/a/6849299/388520
+class lazy_property(object):
+    """Decorator for an attribute whose value is to be lazily computed
+       on first use.  Apply to a method which computes the value.
+       Overwrites itself with the computed value upon invocation."""
+
+    def __init__(self, fget):
+        self.__func__ = fget
+        self.__name__ = fget.__name__
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            obj = cls
+        value = self.__func__(obj)
+        setattr(obj, self.__name__, value)
+        return value
